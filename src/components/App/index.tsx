b32d94485dff1ccb055750691597c579cdea3c86
Jul-Me <memai.julian@gmail.com>
2022-03-01 16:27:38 +0000

    feat: Added React router

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Mainboard from '../Mainboard';
 import Nav from '../Nav';
 
 import './App.scss';
 
-function App() {
+const App = (): JSX.Element => {
+  const renderRoutes = (): JSX.Element => {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Mainboard />} />
+          {/* <Route path="/exercise-planner" element={} /> */}
+          {/* <Route path="/meal-planner" element={} />
+          <Route path="/hydration-tracker" element={} />
+          <Route path="/meditation-portal" element={} /> */}
+        </Routes>
+      </BrowserRouter>
+    );
+  };
+
   return (
     <div className="app">
       <Nav />
-      <Mainboard />
+
+      {renderRoutes()}
     </div>
   );
-}
+};
 
 export default App;
